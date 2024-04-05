9423fdb05c69902f753f764bb9e98040f84a86bd
Matheus N Leite <matheusn06leite@gmail.com>
2024-04-05 10:18:57 -0300

    Primeira aula sobre variaveis, tipos de dados e operadores

---
+++ variaveis_dados_operadores/variavel.py
@@ -0,0 +1,26 @@
+a = 10 #nome da variavel e o tipo de dado
+b = 10 
+a = 20 
+alvo = 192
+
+print(a) #printar o valor de A
+
+print("Hello World") #printando texto
+
+print("Atacando o alvo",alvo) #printando texto + variavel
+
+tipo_string = "\"Legiao\" TDI" #variavel do tipo string, \" para ignorar a formatação e escrever as aspas na minha string
+
+
+tipo_boolean = True #True ou False com letra maiuscula para definir a variavel boolean
+
+
+
+#Converter Tipos de Variáveis
+
+n1="10"
+n2=20
+print(int(n1) + n2) #Converteu string para inteiro
+
+
+print(type(tipo_string)) #imprime na tela o tipo da variavel

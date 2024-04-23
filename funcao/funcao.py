cd81d13a3f873480afa72ba67b1359279f9591cd
Matheus N Leite <matheusn06leite@gmail.com>
2024-04-23 10:44:24 -0300

    Funções

---
+++ funcao/funcao.py
@@ -0,0 +1,20 @@
+def fazer_login(email, senha):
+    print("bem vindo")
+    print("usuario: " + email)
+# def nome_da_funcao(valores que ela vai receber):
+
+fazer_login("matheus@gmail.com","123") #chamando a funcao
+
+
+def hello(name):
+    print("Hello World, " + name)
+
+
+hello("Matheus") 
+
+
+def plus(n1,n2):
+    return n1+n2
+
+resultado = plus(10, 10)
+print(resultado)
